ea002d6743be24c6cb4d09ea5e9ffefd3ee2b189
Bo Garner <garnerbo@gmail.com>
2017-05-21 19:57:58 -0400

    first commit

---
+++ pipeclass.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+class Pipe:
+
+    def __init__(self, name='', maker='', shape='', store='', buyyear=0):
+        Pipe.name = name
+        Pipe.maker = maker
+        Pipe.shape = shape
+        Pipe.store = store
+        Pipe.buyyear = buyyear
